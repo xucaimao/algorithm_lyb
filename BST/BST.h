d67175f60ffafb6884d72bf21e7604c0610c0f20
xcmgj <xcmgj@163.com>
2017-06-25 23:50:07 +0800

    new class BST

--- BST/BST.h
+++ BST/BST.h
@@ -2,6 +2,7 @@
 // Created by xcm on 17/6/25.
 //
 #include <iostream>
+using namespace std;
 
 #ifndef BST_BST_H
 #define BST_BST_H
@@ -26,24 +27,58 @@ private:
     Node* root;
     int count;
 
-    //向以node为根节点的二叉搜索树，插入节点(key,value)
+    //递归形式
+    // 向以node为根节点的二叉搜索树，插入节点(key,value)
     //返回插入新节点后的二叉搜索树的根
     Node* insert(Node* node,Key key,Value value){
         if(node==NULL){
             count++;
             //新生成一个节点并利用构造函数为节点赋值
+            cout<<"add "<<key<<" "<<endl;
             return new Node(key,value);
         }
-        if(key==node->key)
+        if(key == node->key)
             node->value=value;
-        else if(key<node->key)
+        else if(key < node->key)
             node->left=insert(node->left,key,value);
         else
             node->right=insert(node->right,key,value);
 
-        return node;
+       return node;
     }
 
+    void printBST(Node* node){
+        if(node== NULL)
+            return;
+
+        printBST(node->left);
+        cout<< node->key <<" ";
+        printBST(node->right);
+    }
+    //查看在以node为根的二叉搜索树中，是否包含键值为key的节点
+    bool contain(Node* node,Key key){
+        if(node == NULL)
+            return false;
+
+        if(node->key == key )
+            return true;
+        else if(key < node->key)
+            return contain(node->left,key);
+        else
+            return contain(node->right,key);
+
+    }
+
+    Value* search(Node* node,Key key){
+        if(node == NULL)
+            return NULL;
+        if(key == node->key)
+            return &(node->value);
+        else if(key < node->key )
+            return search(node->left,key);
+        else
+            return search(node->right,key);
+    }
 
 public:
     BST(){
@@ -59,8 +94,52 @@ public:
     bool isEmpty(){
         return count==0;
     }
+
+    //插入函数接口，调用递归函数进行插入
     void insert(Key key,Value value){
         root=insert(root,key,value);
+    }
+
+    //非递归的插入函数
+    void insert2(Key key,Value value){
+        Node* p=root;
+        Node* fa;
+        int dir=0;//左右孩子标志，0左1右
+        while(true){
+            if(p==NULL){
+                fa=new Node(key,value);
+                count++;
+                cout<<"add "<<key<<" "<<endl;
+                break;
+            }
+            if(key == p->key){
+                p->value=value;
+                break;
+            }
+            if(key < p->key){
+                fa=p;
+                dir=0;
+                p=p->left;
+            }
+
+            else{
+                fa=p;
+                dir=1;
+                p=p->right;
+            }
+        }
+    }
+
+    bool contain(Key key){
+        return contain(root,key);
+    }
+
+    Value* search(Key key){
+        return search(root,key);
+    }
+
+    void printBST(){
+        printBST(root);
     }
 };
 
--- BST/main.cpp
+++ BST/main.cpp
@@ -1,28 +1,51 @@
+//
+// Created by xcm on 17/6/25.
+//
 #include <iostream>
+#include <cassert>
+#include "BST.h"
+using namespace std;
 
-//二分查找法，在有n个元素的有序数组arr中，查找元素target
-//如果找到，返回元素在数组中的索引(下标)；否则返回-1
-template <typename T>
-int binarySearch(T arr[],int n,T target){
-    //在arr[l...r]中查找target
-    int l=0,r=n-1;
-    while(l<=r){
-        //l=r时，区间还有一个元素
-        int mid=l+(r-l)/2;
-        if(arr[mid]==target)
-            return mid;
-
-        if(target<arr[mid])
-            //在arr[l...mid-1]中查找
-            r=mid-1;
-        else
-            //在arr[mid+1...r]中查找
-            l=mid+1;
-    }
-    return -1;
+//生成有n个元素的随机数组,元素范围为[rangel,ranger]
+int* generateRandomArray(int n,int rangeL,int rangeR){
+    assert(rangeL<=rangeR);
+    int *arr=new int [n];
+    //srand( (unsigned) time(NULL) );
+    //上面的程序会造成两次生成的随机序列相同，因为time返回值只精确到秒，
+    //会造成两次随机函数执行时time的返回值相同的情况，进而使得随机序列相同
+    srand( (unsigned) rand() );
+    for(int i=0;i<n;i++)
+        arr[i]=rand()%(rangeR-rangeL+1)+rangeL;
+    return arr;
 }
 
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    BST<int,int> bst=BST<int,int>();
+    //class  BST<int,int> bst;
+    int arr[]={41,22,58,15,33,50,60,13,28,37,42,53};
+    //        {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11}
+    int n=12;
+    //int* arr=generateRandomArray(n,10,99);
+    cout<<"The data is : "<<endl;
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+    cout<<endl;
+
+    for(int i=0;i<n;i++)
+        bst.insert(arr[i],1);
+
+    int target;
+    while(cin>>target && target!=-1){
+        cout << "The element [ "<<target <<" ]  ";
+        if( bst.contain(target) )
+            cout<<"exist!"<<endl;
+        else
+            cout<<"not exist!"<<endl;
+    }
+
+    //bst.printBST();
+
+
     return 0;
 }
